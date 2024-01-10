82f777babee68626190987f83747ce5214eaa006
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-01-10 19:03:26 +0600

    created endpoint  encode word

--- index.js
+++ index.js
@@ -6,6 +6,29 @@ app.get('/:echo', (req, res) => {
   res.send(`${req.method} ${req.params.echo} log`);
 });
 
+
+const encryption = (text) => {
+  const en = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  const square = [];
+  const key = "password";
+  let word = "";
+
+  for (let i = 0; i < en.length; i++) {
+    square[i] = en.slice(i).concat(en.slice(0, i));
+  }
+
+  for (let i = 0; i < text.length; i++) {
+    word += square[en.indexOf(text[i].toUpperCase())][en.indexOf(key[i % key.length].toUpperCase())];
+  }
+
+  return word;
+}
+
+
+app.get('/encode/:word', (req, res) => {
+  res.send(encryption(req.params.word));
+});
+
 app.listen(port, () => {
   console.log('port on ' + port);
 });
